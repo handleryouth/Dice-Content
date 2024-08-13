9bdf74c3bb9c59b9d5732897b0018dc4db39a51b
Tony David <70279376+handleryouth@users.noreply.github.com>
2024-08-13 10:38:35 +0700

    Initial Commit

--- DiceView/ContentView.swift
+++ DiceView/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var numberOfDice = 1
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            
+            Text("Dice Roller").font(.largeTitle.lowercaseSmallCaps())
+            
+            
+            HStack(spacing: 30) {
+                ForEach(1...numberOfDice, id: \.description) {
+                    _ in
+                    DiceView()
+                }
+            }
+            
+            HStack (spacing: 30) {
+                Button("Remove Dice", systemImage: "minus") {
+                    numberOfDice -= 1
+                }.disabled(numberOfDice == 1)
+                
+                Button("Add Dice", systemImage: "plus") {
+                    numberOfDice += 1
+                }.disabled(numberOfDice == 5)
+                
+                
+            }.padding()
+            
+           
+
+        }.frame(maxWidth: .infinity, maxHeight: .infinity).background(.appBackground).tint(.white)
     }
 }
 
