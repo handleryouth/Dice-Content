cb462f61f4202f2750869538efe69ae9de061f93
Tony David <70279376+handleryouth@users.noreply.github.com>
2024-08-13 09:52:02 +0700

    Initial Commit

---
+++ DiceView/DiceViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceViewApp.swift
+//  DiceView
+//
+//  Created by Tony Gultom on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
